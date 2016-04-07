adbc4ae6e25c911d360cbbff223b08bb879a3fa5
Travis Tucker <t.tuck303@gmail.com>
2016-04-07 11:02:48 -0600

    Adds custom directive to log out a message using a linking function.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 var myApp = angular.module('myApp', []);
+
+myApp.directive("superman", function(){
+  return {
+    restrict: "A",
+    link: function(){
+      console.log("I am superman!");
+    }
+  }
+
+
+});
