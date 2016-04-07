85ffe481807c620a2cebfada433817b92fa38158
Travis Tucker <t.tuck303@gmail.com>
2016-04-07 10:53:16 -0600

    Adds and removes class based on element attribute argument.

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var myApp = angular.module('myApp', []);
 
 myApp.directive('enter', function(){
-  return function(scope, element){
+  return function(scope, element, attrs){
     element.bind("mouseenter", function(){
       console.log("I'm inside of " + element);
+      element.addClass(attrs.enter);
     })
   }
 });
 
 myApp.directive('leave', function(){
-  return function(scope, element){
+  return function(scope, element, attrs){
     element.bind("mouseleave", function(){
       console.log("I'm leaving " + element);
+      element.removeClass(attrs.enter);
     })
   }
 });
