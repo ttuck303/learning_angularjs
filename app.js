b62540a67ff4092aea8fe61a79198e468dfb7d9e
Travis Tucker <t.tuck303@gmail.com>
2016-04-07 10:26:06 -0600

    Adds a console log when the div is entered or left.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 var myApp = angular.module('myApp', []);
+
+myApp.directive('enter', function(){
+  return function(scope, element){
+    element.bind("mouseenter", function(){
+      console.log("I'm inside of " + element);
+    })
+  }
+});
+
+myApp.directive('leave', function(){
+  return function(scope, element){
+    element.bind("mouseleave", function(){
+      console.log("I'm leaving " + element);
+    })
+  }
+});
